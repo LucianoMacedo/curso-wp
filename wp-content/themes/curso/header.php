4a64471153054cde08bb9127d86131d371b3b547
Luciano Macedo <luciano.macedo@duo.studio>
2024-07-23 17:07:20 -0300

    criacao do header e footer

---
+++ wp-content/themes/curso/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso Wordpress</title>
+</head>
+
+<body <?php body_class() ?>>
+    <header>
+        <section class="top-bar">
+            <div class="social-media-icons"></div>
+            <div class="search"></div>
+        </section>
+        <section class="menu-area">
+            <section class="logo"></section>
+            <nav class="menu"></nav>
+        </section>
+    </header>
